7007e4b593e854c907a90cf55e2b020bc138034e
Moremaster123 <juancarlin01@hotmail.com>
2026-09-16 15:45:43 -0600

    Crea biblioteca musical con React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './Components/Header';
+import Song from './Components/Song';
+
+class App extends Component {
+
+    componentDidMount() {
+        console.log('La aplicación se ha cargado correctamente');
+    }
+
+    render() {
+        return (
+            <div className="App">
+
+                <Header />
+
+                <main className="music-library">
+
+                    <h2>Mis canciones</h2>
+
+                    <Song
+                        title="Billie Jean"
+                        artist="Michael Jackson"
+                        album="Thriller"
+                        duration="4:54"
+                    />
+
+                    <Song
+                        title="Bohemian Rhapsody"
+                        artist="Queen"
+                        album="A Night at the Opera"
+                        duration="5:55"
+                    />
+
+                    <Song
+                        title="Imagine"
+                        artist="John Lennon"
+                        album="Imagine"
+                        duration="3:03"
+                    />
+
+                </main>
+
+            </div>
+        );
+    }
 }
 
 export default App;
